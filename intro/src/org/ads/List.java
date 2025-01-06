9073d32effcc408345c9f7d39aed3d9db8cde73f
Daniel Rezende <daniel.rezende@ifmt.edu.br>
2025-01-06 18:14:06 -0400

    Primeiro commit

---
+++ intro/src/org/ads/List.java
@@ -0,0 +1,9 @@
+public class List {
+    public static void main(String[] args) throws Exception {
+        int maxList = 5;
+        int i=0;
+        for(i=0; i < maxList; i++){
+            System.out.println(i);
+        } 
+    }
+}
